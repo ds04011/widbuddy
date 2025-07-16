e72dcb296db836add514771b22ff97acad4ed91a
ds04011 <tommyhw97@gmail.com>
2025-07-16 16:54:26 +0900

    ds04011 - 최초 커밋, hello 출력 확인.

---
+++ src/main/java/com/ds04011/widbuddy/WidbuddyApplication.java
@@ -0,0 +1,13 @@
+package com.ds04011.widbuddy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WidbuddyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WidbuddyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ds04011/widbuddy/WidbuddyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ds04011.widbuddy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WidbuddyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
